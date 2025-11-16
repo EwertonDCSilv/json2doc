efd510dfb96f4c303d4ef381e4ce0e709cca167a
ewert <ewerton.silva@nhecotech.com>
2025-11-15 21:13:23 -0300

    Add lib to local compile

---
+++ include/pugiconfig.hpp
@@ -0,0 +1,5 @@
+// Minimal stub for pugiconfig.hpp to allow compilation with Emscripten/WASM
+#ifndef PUGICONFIG_HPP
+#define PUGICONFIG_HPP
+// No configuration needed for stub
+#endif // PUGICONFIG_HPP
